832fc11fc50da080c608b733e665d4d969a35c10
chaitanyabansal1 <chaitanyabansalchd@gmail.com>
2026-07-11 14:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.05%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -5,22 +5,7 @@ class Solution {
             if(target<=nums[i]){
                 return i;
             }
-            // else if(target<nums[0]){
-            //     return 0;
-            // }
         }
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=1;j<nums.length;j++){
-        //     if(target>nums[i] && target<nums[j]){
-        //         return i+1;
-        //     }
-        //     else if(j==nums.length-1 && target>nums[j]){
-        //         return j+1;
-
-        //     }
-
-        // }
-    // }
     return nums.length;
     }
 }
