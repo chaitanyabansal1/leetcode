a567d3780c7dc921f6138936b31a2ebdabae915c
chaitanyabansal1 <chaitanyabansalchd@gmail.com>
2026-08-04 11:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (83.01%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]==target){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
